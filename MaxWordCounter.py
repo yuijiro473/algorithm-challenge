f298dd01252b548f6f3981e2546cb752e4cd9339
yuijiro473 <yuichiro.aoki@asia-quest.jp>
2023-05-30 09:59:19 +0900

    新規作成

---
+++ MaxWordCounter.py
@@ -0,0 +1,11 @@
+def most_frequent_word(text):
+    # ここにコードを書いてください
+    pass
+
+def test_most_frequent_word():
+    assert most_frequent_word("the quick brown fox jumps over the lazy dog") == "the"
+    assert most_frequent_word("hello world world world") == "world"
+    assert most_frequent_word("a") == "a"
+    print("All test cases passed")
+
+test_most_frequent_word()
